2040e6a0f02c8d0ab8a1273f32dac76aed64d6ee
XeniaSv <savelicheva123@gmail.com>
2022-03-24 20:21:20 +0300

    [pages] Create top bar
    * Implement Topbar
    * Create stylesTopbar

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,153 @@
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Paper from '@mui/material/Paper';
+import MenuItem from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
+import MenuIcon from '@mui/icons-material/Menu';
+import Popper from '@mui/material/Popper';
+import {Link} from 'react-router-dom';
+// import {AuthContext} from "../../authContext/AuthContext";
+// import {logout} from "../../authContext/AuthActions";
+import useStyles from "./stylesTopbar";
+import Grow from '@mui/material/Grow';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+
+
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const pages = ['Products', 'Pricing', 'Blog'];
+
+export default function Topbar() {
+    const classes = useStyles();
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef(null);
+
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+    };
+
+    const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    function handleListKeyDown(event) {
+        if (event.key === 'Tab') {
+            event.preventDefault();
+            setOpen(false);
+        } else if (event.key === 'Escape') {
+            setOpen(false);
+        }
+    }
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+
+        <div className={classes.topbar}>
+            <div className={classes.topbarWrapper}>
+                <div>
+                    <span className={classes.menu}>
+
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            aria-label="menu"
+                            sx={{mr: 2}}
+                            onClick={handleToggle}
+                            ref={anchorRef}
+                            aria-controls={open ? 'composition-menu' : undefined}
+                            aria-expanded={open ? 'true' : undefined}
+                            aria-haspopup="true"
+                        >
+                            <MenuIcon className={classes.menu}/>
+                        </IconButton>
+                         <Popper
+                             open={open}
+                             anchorEl={anchorRef.current}
+                             role={undefined}
+                             placement="bottom-start"
+                             transition
+                             disablePortal
+
+                         >
+          {({TransitionProps, placement}) => (
+              <Grow
+                  {...TransitionProps}
+                  style={{
+                      transformOrigin:
+                          placement === 'bottom-start' ? 'left top' : 'left bottom',
+                  }}
+              >
+                  <Paper>
+                      <ClickAwayListener onClickAway={handleClose}>
+                          <MenuList
+                              className={classes.menuList}
+                              autoFocusItem={open}
+                              id="composition-menu"
+                              aria-labelledby="composition-button"
+                              onKeyDown={handleListKeyDown}
+                          >
+                              <MenuItem onClick={handleClose}>
+                                  <Link to="/admin" className="link">
+                                      Start
+                                  </Link>
+                              </MenuItem>
+                              <MenuItem onClick={handleClose}>
+                                  <Link to="/adminUsers" className="link">
+                                      Users List
+                                  </Link></MenuItem>
+                              <MenuItem onClick={handleClose}>
+                                  <Link to="/adminMovies" className="link">
+                                      Movies List
+                                  </Link>
+                              </MenuItem>
+                              <MenuItem onClick={handleClose}>
+                                  <Link to="/adminSeries" className="link">
+                                      Series List
+                                  </Link>
+                              </MenuItem>
+                              <MenuItem onClick={handleClose}>
+                                  <Link to="/adminBooks" className="link">
+                                      Books List
+                                  </Link>
+                              </MenuItem>
+                              <MenuItem onClick={handleClose}>
+                                  <Link to="/" className="link">
+                                      Homepage
+                                  </Link>
+                              </MenuItem>
+                          </MenuList>
+                      </ClickAwayListener>
+                  </Paper>
+              </Grow>
+          )}
+        </Popper>
+                    </span>
+
+
+                    <span className={classes.logo}>Reviewer admin</span>
+                </div>
+            </div>
+
+        </div>
+    );
+}
---
+++ src/components/topbar/stylesTopbar.js
@@ -0,0 +1,46 @@
+import {makeStyles} from "@material-ui/styles";
+
+const useStyles = makeStyles({
+    topbar: {
+        width: '100%',
+        height: '50px',
+        position: 'sticky',
+        top: '0',
+        zIndex: '999',
+        background: 'linear-gradient(90deg, #69cfbc  0%, #9FACE6 100%)',
+
+    },
+
+    topbarWrapper: {
+        height: '100%',
+        padding: '0px 20px',
+        display: ' flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+
+    logo: {
+        fontWeight: 'bold',
+        fontSize: '30px',
+        color: 'white',
+        "@media (max-width: 820px)": {
+            fontSize: '20px',
+        }
+    },
+    menu: {
+        "&.MuiSvgIcon-root": {
+            color: 'white'
+        },
+        "@media (min-width: 820px)": {
+            display: 'none',
+        }
+    },
+    menuList: {
+        ".MuiList-root": {
+            margin: '10px'
+        }
+    }
+
+});
+
+export default useStyles;
