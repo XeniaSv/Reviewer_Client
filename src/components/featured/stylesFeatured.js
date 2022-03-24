18f17210e54a620b9bf87c6a47a8755d503c9ea9
XeniaSv <savelicheva123@gmail.com>
2022-03-24 20:23:44 +0300

    [home-page] Create featured
    * Implement Featured
    * Create stylesFeatured

---
+++ src/components/featured/stylesFeatured.js
@@ -0,0 +1,34 @@
+import {makeStyles} from "@material-ui/styles";
+
+const useStyles = makeStyles({
+    nav: {
+        marginTop: '80px',
+        marginBottom: '80px',
+        width: '100%',
+    },
+    tab: {
+
+        "& .MuiTab-root": {
+            fontSize: '25px',
+            fontWeight: 'bold',
+            color: 'lightgrey',
+            "@media (max-width: 820px)": {
+                fontSize: '18px',
+            }
+        },
+        "& .MuiTabs-indicator": {
+            backgroundColor: "#a9c8d6",
+
+        },
+        "& .Mui-selected": {
+            color: '#a9c8d6!important',
+
+        },
+
+    }
+
+});
+export default useStyles;
+
+
+
