ca4cedadf540d2c2df16ed5250b3098a7394dd79
XeniaSv <savelicheva123@gmail.com>
2022-03-24 20:35:12 +0300

    [index] Create index page
    * Implement index
    * Delete logo.svg

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {AuthContextProvider} from "./context/authContext/AuthContext";
+import {MovieContextProvider} from "./context/movieContext/MovieContext";
+import {SeriesContextProvider} from "./context/seriesContext/SeriesContext";
+import {BookContextProvider} from "./context/bookContext/BookContext";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <AuthContextProvider>
+            <MovieContextProvider>
+                <SeriesContextProvider>
+                    <BookContextProvider>
+                        <App/>
+                    </BookContextProvider>
+                </SeriesContextProvider>
+            </MovieContextProvider>
+        </AuthContextProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
