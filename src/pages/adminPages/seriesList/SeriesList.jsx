f33f5e3bacacf80d0fa02f887897ee127a0313fe
XeniaSv <savelicheva123@gmail.com>
2022-03-24 18:40:06 +0300

    [admin-page] Create series list page
    * Implement SeriesList
    * Create stylesSeriesList

---
+++ src/pages/adminPages/seriesList/SeriesList.jsx
@@ -0,0 +1,111 @@
+import React, {useContext, useEffect} from 'react';
+import {DataGrid} from "@material-ui/data-grid";
+import {Link} from "react-router-dom";
+import {SeriesContext} from "../../../context/seriesContext/SeriesContext";
+import {deleteSeries, getSeries} from "../../../context/seriesContext/apiCalls";
+import Button from "@material-ui/core/Button";
+import useStyles from './stylesSeriesList';
+import Topbar from "../../../components/topbar/Topbar";
+import Sidebar from "../../../components/sidebarAdmin/SidebarAdmin";
+import Grid from "@mui/material/Grid";
+import {styled} from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+
+const Item = styled(Paper)(({theme}) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+
+export default function SeriesList() {
+    const classes = useStyles();
+    const {series, dispatch} = useContext(SeriesContext);
+
+    useEffect(() => {
+        getSeries(dispatch);
+    }, [dispatch])
+
+    const handleDelete = (id) => {
+        deleteSeries(id, dispatch);
+    };
+
+
+    const columns = [
+        {field: "id", headerName: "ID", width: 90},
+        {
+            field: "title",
+            headerName: "Title",
+            width: 200,
+            renderCell: (params) => {
+                return (
+                    <div className={classes.productListItem}>
+                        <img className={classes.productListImg} src={params.row.imgSm} alt=""/>
+                        {params.row.title}
+                    </div>
+                );
+            },
+        },
+        {field: "director", headerName: "Director", width: 120},
+        {field: "year", headerName: "Year", width: 120},
+        {field: "duration", headerName: "Duration", width: 120},
+        {field: "genre", headerName: "Genre", width: 120},
+        {field: "limit", headerName: "limit", width: 120},
+        {field: "cast", headerName: "Cast", width: 120},
+        {field: "desc", headerName: "Description", width: 200},
+
+        // {
+        //     field: "action",
+        //     headerName: "Action",
+        //     width: 150,
+        //     renderCell: (params) => {
+        //
+        //         return (
+        //             <>
+        //                 <Link to={{pathname: "/movie/" + params.row._id, movie:params.row}}>
+        //                     <button className={classes.productListEdit}>Edit</button>
+        //                 </Link>
+        //                 <DeleteOutline
+        //                     className={classes.productListDelete}
+        //                     onClick={() => handleDelete(params.row._id)}
+        //                 />
+        //             </>
+        //         );
+        //     },
+        // },
+    ];
+
+    return (
+        <>
+            <Topbar/>
+            <Link to={{pathname: '/adminNewItem', search: '?type=series'}} className='link'>
+                <Button className={classes.button} variant="outlined">ADD NEW TV SERIES</Button>
+            </Link>
+            <Grid className={classes.wrapper} container spacing={2}>
+                <Grid className={classes.hide} item xs={2}>
+                    <Item><Sidebar/></Item>
+                </Grid>
+                <Grid item xs={10}>
+                    <Item>
+                        <div className={classes.productList}>
+
+                            <DataGrid
+                                rows={series}
+                                disableSelectionOnClick
+                                columns={columns}
+                                pageSize={8}
+                                checkboxSelection
+                                getRowId={(r) => r.id}
+                                className={classes.table}
+                            />
+                        </div>
+                    </Item>
+                </Grid>
+            </Grid>
+
+
+        </>
+
+    );
+}
