438b0bb68eebed0005c5c0cccb8ba91d4d07299f
XeniaSv <savelicheva123@gmail.com>
2022-03-24 20:29:20 +0300

    [user-page] Create movie reviews page
    * Implement MovieReviews
    * Create stylesMovieReviews

---
+++ src/pages/userPages/movieReviews/stylesMovieReviews.js
@@ -0,0 +1,25 @@
+import {makeStyles} from "@material-ui/styles";
+
+const useStyles = makeStyles({
+    table: {
+        color: '#406170!important',
+        backgroundColor: 'rgba(235, 242, 245, 0.3)'
+    },
+    edit: {
+        border: 'none',
+        borderRadius: '10px',
+        padding: '5px 10px',
+        backgroundColor: 'rgba(144, 156, 209)',
+        color: 'white',
+        cursor: 'pointer',
+        marginRight: '20px'
+    },
+
+    delete: {
+        color: '#406170',
+        cursor: 'pointer'
+    }
+
+});
+
+export default useStyles;
