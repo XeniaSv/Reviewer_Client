a826edeb798b9872ffff6007a05d06676ae9d593
XeniaSv <savelicheva123@gmail.com>
2022-03-24 23:43:23 +0300

    [item-page] Modify item page
    * Fill item information
    * Fill item rating
    * Fill user item rating

--- src/pages/itemPage/ItemPage.jsx
+++ src/pages/itemPage/ItemPage.jsx
@@ -1,38 +1,171 @@
 import './stylesItemPage'
-import {ArrowBackOutlined, Star, ThumbDownOutlined, ThumbUpAltOutlined} from "@mui/icons-material";
-import {Link, useLocation} from "react-router-dom";
+import {Star} from "@mui/icons-material";
+import {useLocation} from "react-router-dom";
 import Navbar from "../../components/navbar/Navbar";
-import {Box, Grid} from "@mui/material";
-import TextField from "@mui/material/TextField";
-import React from "react";
+import {Grid} from "@mui/material";
+import React, {useEffect, useState} from "react";
 import useStyles from "../itemPage/stylesItemPage";
 import ReviewList from "../../components/reviewList/ReviewList";
 import Rating from '@mui/material/Rating';
-import Button from "@mui/material/Button";
-import AddIcon from "@mui/icons-material/Add";
 import AddReview from "../../components/addReview/AddReview";
 
-function ItemPage() {
-    const location = useLocation();
-    console.log(location);
+import {getMovie, getMovieRating, getUserMovieRating, putMovieRating} from "../../context/movieContext/apiCalls";
+import {getBook, getBookRating, getUserBookRating, putBookRating} from "../../context/bookContext/apiCalls";
+import {
+    getOneSeries,
+    getSeriesRating,
+    getUserSeriesRating,
+    putSeriesRating
+} from "../../context/seriesContext/apiCalls";
 
+function ItemPage() {
     const {search} = useLocation();
 
     const type = new URLSearchParams(search).get('type');
     const itemId = new URLSearchParams(search).get('id');
-    console.log(type);
-    console.log(itemId);
+
+    const [item, setItem] = useState({});
+    const [rating, setRating] = useState(0);
+    const [userRating, setUserRating] = useState(0);
+
+    useEffect(async () => {
+        await fillItem();
+        await fillRating();
+        await fillUserRating();
+    }, [type, itemId, userRating]);
+
+    const fillItem = async () => {
+        switch (type) {
+            case "movie":
+                const movieRes = await getMovie(itemId);
+                if (movieRes.status === 200) {
+                    setItem(movieRes.data);
+                    return;
+                }
+                alert(movieRes.statusText);
+                break;
+            case "series":
+                const seriesRes = await getOneSeries(itemId);
+                if (seriesRes.status === 200) {
+                    setItem(seriesRes.data);
+                    return;
+                }
+                alert(seriesRes.statusText);
+                break;
+            case "book":
+                const bookRes = await getBook(itemId);
+                if (bookRes.status === 200) {
+                    setItem(bookRes.data);
+                    return;
+                }
+                alert(bookRes.statusText);
+                break;
+            default:
+                alert('This type does not exist');
+                break;
+        }
+    };
+
+    const fillRating = async () => {
+        switch (type) {
+            case "movie":
+                const movieRatingRes = await getMovieRating(itemId);
+                if (movieRatingRes.status === 200) {
+                    setRating(movieRatingRes.data.rate);
+                    return;
+                }
+                alert(movieRatingRes.statusText);
+                break;
+            case "series":
+                const seriesRatingRes = await getSeriesRating(itemId);
+                if (seriesRatingRes.status === 200) {
+                    setRating(seriesRatingRes.data.rate);
+                    return
+                }
+                alert(seriesRatingRes.statusText);
+                break;
+            case "book":
+                const bookRatingRes = await getBookRating(itemId);
+                if (bookRatingRes.status === 200) {
+                    setRating(bookRatingRes.data.rate);
+                    return;
+                }
+                alert(bookRatingRes.statusText);
+                break;
+            default:
+                alert('This type does not exist');
+                break;
+        }
+    };
+
+    const fillUserRating = async () => {
+        switch (type) {
+            case "movie":
+                const movieUserRatingRes = await getUserMovieRating(itemId);
+                if (movieUserRatingRes.status === 200) {
+                    setUserRating(movieUserRatingRes.data.rate);
+                    return
+                }
+                break;
+            case "series":
+                const seriesUserRatingRes = await getUserSeriesRating(itemId);
+                if (seriesUserRatingRes.status === 200) {
+                    setUserRating(seriesUserRatingRes.data.rate);
+                    return
+                }
+                break;
+            case "book":
+                const bookUserRatingRes = await getUserBookRating(itemId);
+                if (bookUserRatingRes.status === 200) {
+                    setUserRating(bookUserRatingRes.data.rate);
+                    return
+                }
+                break;
+            default:
+                alert('This type does not exist');
+                break;
+        }
+    };
+
+    const handleUserRating = async (e, value) => {
+        switch (type) {
+            case "movie":
+                const movieUserRatingRes = await putMovieRating(itemId, value);
+                if (movieUserRatingRes.status === 201) {
+                    setUserRating(movieUserRatingRes.data.rate);
+                    return
+                }
+                break;
+            case "series":
+                const seriesUserRatingRes = await putSeriesRating(itemId, value);
+                if (seriesUserRatingRes.status === 201) {
+                    setUserRating(seriesUserRatingRes.data.rate);
+                    return
+                }
+                break;
+            case "book":
+                const bookUserRatingRes = await putBookRating(itemId, value);
+                if (bookUserRatingRes.status === 201) {
+                    setUserRating(bookUserRatingRes.data.rate);
+                    return
+                }
+                break;
+            default:
+                alert('This type does not exist');
+                break;
+        }
+    }
 
     const classes = useStyles();
     return (
         <div className={classes.itemPage}>
             <Navbar/>
-            <Grid   container direction='column' className={classes.container}>
-                <Grid className={classes.item} >
-                        <img
-                            className={classes.image}
-                            src="https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/eed1de3a-5400-43b3-839e-22490389bf54/300x450"
-                        />
+            <Grid container direction='column' className={classes.container}>
+                <Grid className={classes.item}>
+                    <img
+                        className={classes.image}
+                        src={item.img}
+                    />
 
                 </Grid>
 
@@ -41,107 +174,102 @@ function ItemPage() {
                     direction="column"
                     justifyContent="center"
                     alignItems="flex-start"
-                    className={classes.info}
+                    className={classes.info}>
 
-                >
                     <Grid container direction="row"
                           justifyContent="flex-start"
                           alignItems="center">
-                        <Grid >
+                        <Grid>
                             <div className={classes.ratingInfo}>
-                                <span className={classes.rating}>124</span>
+                                <span className={classes.rating}>{rating}</span>
                                 <Star fontSize="large" className={classes.icon}/>
                             </div>
                         </Grid>
-                        <Grid >
-                            <Rating className={classes.ratingStar} name="size-large" defaultValue={0} size="large"/>
+                        <Grid>
+                            <Rating className={classes.ratingStar} value={userRating}
+                                    onChange={handleUserRating}
+                                    name="size-large" defaultValue={0} size="large"/>
                         </Grid>
-
-
                     </Grid>
+
                     <Grid xs={5} container className={classes.infoItem}>
                         {type === 'book' ?
                             <>
-                                <Grid >
+                                <Grid>
                                     <div>
                                         <h3 className={classes.infoTitle} style={{marginTop: '0'}}>TITLE</h3>
-                                        <span>Movie title</span>
+                                        <span>{item.title}</span>
                                     </div>
                                     <div>
                                         <h3 className={classes.infoTitle}>AUTHOR</h3>
-                                        <span>Movie director</span>
+                                        <span>{item.author}</span>
                                     </div>
                                     <div>
                                         <h3 className={classes.infoTitle}>THE YEAR OF PUBLISHING</h3>
-                                        <span>Movie year</span>
+                                        <span>{item.year}</span>
                                     </div>
                                     <div>
                                         <h3 className={classes.infoTitle}>NUMBER OF PAGES</h3>
-                                        <span>Movie duration</span>
+                                        <span>{item.pages}</span>
                                     </div>
-
                                 </Grid>
-                                <Grid >
+
+                                <Grid>
                                     <div>
                                         <h3 className={classes.infoTitle} style={{marginTop: '0'}}>LANGUAGE</h3>
-                                        <span>Movie limit</span>
+                                        <span>{item.language}</span>
                                     </div>
 
                                     <div>
                                         <h3 className={classes.infoTitle}>GENRE</h3>
-                                        <span>Movie genre</span>
+                                        <span>{item.genre !== undefined ? item.genre.join(", ") : ""}</span>
                                     </div>
                                     <div>
                                         <h3 className={classes.infoTitle}>DESCRIPTION</h3>
-                                        <span>Movie description</span>
+                                        <span>{item.desc}</span>
                                     </div>
                                 </Grid>
                             </>
-
-
                             :
                             <>
-                            <Grid >
-                                <div>
-                                    <h3 className={classes.infoTitle} style={{marginTop: '0'}}>TITLE</h3>
-                                    <span>Movie title</span>
-                                </div>
-                                <div>
-                                    <h3 className={classes.infoTitle}>DIRECTOR</h3>
-                                    <span>Movie director</span>
-                                </div>
-                                <div>
-                                    <h3 className={classes.infoTitle}>YEAR</h3>
-                                    <span>Movie year</span>
-                                </div>
-                                <div>
-                                    <h3 className={classes.infoTitle}>DURATION</h3>
-                                    <span>Movie duration</span>
-                                </div>
+                                <Grid>
+                                    <div>
+                                        <h3 className={classes.infoTitle} style={{marginTop: '0'}}>TITLE</h3>
+                                        <span>{item.title}</span>
+                                    </div>
+                                    <div>
+                                        <h3 className={classes.infoTitle}>DIRECTOR</h3>
+                                        <span>{item.director}</span>
+                                    </div>
+                                    <div>
+                                        <h3 className={classes.infoTitle}>YEAR</h3>
+                                        <span>{item.year}</span>
+                                    </div>
+                                    <div>
+                                        <h3 className={classes.infoTitle}>DURATION</h3>
+                                        <span>{item.duration}</span>
+                                    </div>
+                                </Grid>
 
-                            </Grid>
-                            <Grid >
-                                <div>
-                                    <h3 className={classes.infoTitle} style={{marginTop: '0'}}>LIMIT</h3>
-                                    <span>Movie limit</span>
-                                </div>
-                                <div>
-                                    <h3 className={classes.infoTitle}>MAIN ACTORS</h3>
-                                    <span>Movie main actors</span>
-                                </div>
-                                <div>
-                                    <h3 className={classes.infoTitle}>GENRE</h3>
-                                    <span>Movie genre</span>
-                                </div>
-                                <div>
-                                    <h3 className={classes.infoTitle}>DESCRIPTION</h3>
-                                    <span>Movie description</span>
-                                </div>
-                            </Grid>
-
+                                <Grid>
+                                    <div>
+                                        <h3 className={classes.infoTitle} style={{marginTop: '0'}}>LIMIT</h3>
+                                        <span>{item.limit}</span>
+                                    </div>
+                                    <div>
+                                        <h3 className={classes.infoTitle}>MAIN ACTORS</h3>
+                                        <span>{item.cast !== undefined ? item.cast.join(", ") : ""}</span>
+                                    </div>
+                                    <div>
+                                        <h3 className={classes.infoTitle}>GENRE</h3>
+                                        <span>{item.genre !== undefined ? item.genre.join(", ") : ""}</span>
+                                    </div>
+                                    <div>
+                                        <h3 className={classes.infoTitle}>DESCRIPTION</h3>
+                                        <span>{item.desc}</span>
+                                    </div>
+                                </Grid>
                             </>
-
-
                         }
                     </Grid>
                 </Grid>
@@ -149,7 +277,7 @@ function ItemPage() {
             <ReviewList/>
             <AddReview/>
         </div>
-    );
+    )
 }
 
 export default ItemPage;
