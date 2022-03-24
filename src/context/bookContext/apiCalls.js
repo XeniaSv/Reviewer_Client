f88296eabb06886293f2d4da9018c4ecd41d2727
XeniaSv <savelicheva123@gmail.com>
2022-03-24 23:40:52 +0300

    [book-context] Add new api to book context
    * Add new api getBook
    * Add new api getBookRating
    * Add new api getUserBookRating
    * Add new api putBookRating

--- src/context/bookContext/apiCalls.js
+++ src/context/bookContext/apiCalls.js
@@ -21,6 +21,30 @@ export const getBooks = async (dispatch) => {
     }
 };
 
+export const getBook = async (id) => {
+    try {
+        return await $api.get(`/books/${id}`);
+    } catch (e) {
+        return e.response;
+    }
+};
+
+export const getBookRating = async (id) => {
+    try {
+        return await $api.get(`/books/rate/${id}`);
+    } catch (e) {
+        return e.response;
+    }
+};
+
+export const getUserBookRating = async (id) => {
+    try {
+        return await $api.get(`/books/rate/${JSON.parse(localStorage.getItem('user')).user.id}/${id}`)
+    } catch (e) {
+        return e.response;
+    }
+};
+
 //create
 export const createBook = async (book, dispatch) => {
     dispatch(createBookStart());
@@ -42,5 +66,19 @@ export const deleteBook = async (id, dispatch) => {
         dispatch(deleteBookSuccess(id));
     } catch (e) {
         dispatch(deleteBookFailure());
+    }
+};
+
+//put
+export const putBookRating = async (bookId, rate) => {
+    try {
+        return await $api.put(`/books/rate/${bookId}`, {
+            user: JSON.parse(localStorage.getItem('user')).user.id,
+            item: bookId,
+            onItem: 'Book',
+            rate: rate
+        });
+    } catch (e) {
+        return e.response;
     }
 };
