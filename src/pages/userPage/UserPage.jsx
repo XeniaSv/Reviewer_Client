70e11e8a5259f0e3ed32c86f060490cc6966b7f7
XeniaSv <savelicheva123@gmail.com>
2022-03-24 20:31:14 +0300

    [user-page] Create user page
    * Implement UserPage
    * Create stylesUserPage

---
+++ src/pages/userPage/UserPage.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Navbar from "../../components/navbar/Navbar";
+import SidebarUser from "../../components/sidebarUser/SidebarUser";
+import {styled} from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Profile from "../userPages/profile/Profile";
+//import {useStyles}from "../../pages/userPage/stylesUserPage";
+
+const Item = styled(Paper)(({theme}) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+
+export default function UserPage(props) {
+    //const classes = useStyles();
+    return (
+        <>
+            <Navbar/>
+            <Grid style={{marginTop: '70px'}} container spacing={2}>
+                <Grid item xs={2}>
+                    <Item><SidebarUser/></Item>
+                </Grid>
+                <Grid item xs={10}>
+                    <Item><Profile/></Item>
+                </Grid>
+            </Grid>
+
+        </>
+    );
+}
