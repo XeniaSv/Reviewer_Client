8a4a80679c2fa5bc3b32e6fac14de796744cd348
XeniaSv <savelicheva123@gmail.com>
2022-04-28 23:43:12 +0300

    [pages] Implement search page

---
+++ src/pages/searchPage/stylesSearchPage.js
@@ -0,0 +1,17 @@
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+    searchPage: {
+        background: 'linear-gradient(90deg, #69cfbc  0%, #9FACE6 100%)',
+
+    },
+    container:{
+        background:'white',
+        width:'80%',
+        margin:'0 auto',
+        height:'1000px'
+    },
+
+});
+
+export default useStyles;
