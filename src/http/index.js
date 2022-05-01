0b508ca242751066cdfa02eb8839cd6d017e877a
XeniaSv <savelicheva123@gmail.com>
2022-05-01 13:47:32 +0300

    [http] Implement readonly http

--- src/http/index.js
+++ src/http/index.js
@@ -8,25 +8,9 @@ const $api = axios.create({
 });
 
 $api.interceptors.request.use((config) => {
-   config.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("user")).accessToken}`;
-   return config;
+    if (localStorage.getItem("user") !== 'null')
+        config.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("user")).accessToken}`;
+    return config;
 });
-
-// $api.interceptors.response.use((config) =>{
-//     return config
-// }, async (error) => {
-//     const originalRequest = error.config;
-//     if (error.response.status === 401 && error.config && !error.config._isRetry){
-//         originalRequest._isRetry = true;
-//         try {
-//             const response = await axios.get(`${API_URL}/refresh`, {withCredentials: true});
-//             localStorage.setItem('token', response.data.accessToken);
-//             return $api.request(originalRequest);
-//         } catch (e) {
-//             console.log('NOT LOG IN');
-//         }
-//     }
-//     throw error;
-// })
 
 export default $api;
