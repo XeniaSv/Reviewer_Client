1ff2682b57cb28488934379648d0bb36c0366865
XeniaSv <savelicheva123@gmail.com>
2022-05-01 14:02:20 +0300

    [http] Modify http

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = process.env.API_URL;
+export const API_URL = 'https://morning-badlands-79922.herokuapp.com/api';
 
 const $api = axios.create({
     withCredentials: true,
