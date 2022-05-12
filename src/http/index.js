40a183f32f09ff5552e28c4bb10f73cdaad8f3a8
XeniaSv <savelicheva123@gmail.com>
2022-05-12 19:25:44 +0300

    [http] Get URL from process.ENV

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'https://morning-badlands-79922.herokuapp.com/api';
+export const API_URL = process.env.REACT_APP_API_URL;
 
 const $api = axios.create({
     withCredentials: true,
